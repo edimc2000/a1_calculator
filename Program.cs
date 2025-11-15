8a97dfad467c479557a82573b11831bc5ecb68e3
Eddie C. <edimc2000@gmail.com>
2025-11-14 23:24:33 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+
+        enum Operation
+        {
+            Add, 
+            Subtract,
+            Multiply, 
+            Divide, 
+            Power
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator App ");
+            OperationMenu();
+
+
+
+        }
+
+        static void OperationMenu() {
+            Operation[] operations = (Operation[])Enum.GetValues(typeof(Operation));
+            String[] symbols = { "(+)", "(-)", "(*)", "(/)", "(^)" };
+
+            Console.Write("\nType");
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.Write($"\t{i+1}   - to {operations[i]}     \t{symbols[i]} \n");
+            }
+
+
+        }
+    }
+
+
+}
