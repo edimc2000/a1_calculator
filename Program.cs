1edfad9c0b4c6121218c0edceb306d1c3b098ffe
Eddie C. <edimc2000@gmail.com>
2025-11-15 23:19:49 -0600

    constants implemented

--- Program.cs
+++ Program.cs
@@ -5,20 +5,30 @@
 
         static void Main(string[] args)
         {
-            Utility.DisplayTitle("Calculator", "all");
-            
-            
-            
-            
-
             bool validInput = false;
+            int attemptCounter = 0;
+            int maxAttempt = 5;
+            Utility.DisplayTitle("Calculator", "all");
+            Utility.OperationMenu();
 
-
-            while (!validInput) 
+            while (!validInput)
             {
+                String choice = Utility.OperationChoice();
+                int operation = Utility.ValidateInput(choice);
+                attemptCounter++;
 
-               String choice = Utility.OperationMenu();
-               validInput =  Utility.ValidateInput(choice);
+                if (operation != 0) 
+                {
+                    validInput = true; 
+                }
+                                
+                if (attemptCounter >= maxAttempt)
+                {
+                    validInput = true;
+                    Console.SetCursorPosition(29, Console.CursorTop);
+                    Console.WriteLine($"{Utility.ErrorColor} The maximum number of attempts has been reached. " +
+                        $"{attemptCounter} / {maxAttempt} {Utility.ResetColor}");
+                }
 
             }
 
--- Utility.cs
+++ Utility.cs
@@ -1,129 +1,145 @@
 ﻿using System;
+using System.Xml.Linq;
 
 
 namespace Calculator
 {
 
-    enum Operation
+
+
+  enum Operation
+  {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Power
+  }
+
+
+  public static class Utility
+  {
+    public const string ResetColor = "\u001b[39m\u001b[49m"; // reset background and foreground console colors
+    public const string ErrorColor = "\u001b[48;2;255;255;255;38;2;255;0;0m"; // set white background and red foreground
+
+    public static void OperationMenu()
     {
-        Add,
-        Subtract,
-        Multiply,
-        Divide,
-        Power
+      Operation[] operations = (Operation[])Enum.GetValues(typeof(Operation));
+      String[] symbols = { "(+)", "(-)", "(*)", "(/)", "(^)" };
+
+      DisplayTitle("", "top");
+
+      for (int i = 0; i < operations.Length; i++)
+      {
+        string tabStops = i > 0 ? new string(' ', 14) : "   Type\t";
+        string centerThis = $"{tabStops}{i + 1}   - to {operations[i]}     \t{symbols[i]}    ";
+        Console.WriteLine(PrintCenteredTitle(centerThis, 29));
+        
+      }
+      DisplayTitle("", "bottom");
+
+
     }
 
-    public static class Utility
+
+    public static string OperationChoice()
     {
-        public static string OperationMenu()
+
+      Console.Write("  Enter your choice \t: ");
+      string response = Console.ReadLine() ?? "100";
+      return response;
+    }
+
+
+
+    public static int ValidateInput(string input)
+    {
+
+      int position = 29;
+
+      try
+      {
+        int result = Convert.ToInt32(input);
+
+        if (result >= 1 && result <= 5)
         {
-            Operation[] operations = (Operation[])Enum.GetValues(typeof(Operation));
-            String[] symbols = { "(+)", "(-)", "(*)", "(/)", "(^)" };
-
-            DisplayTitle("", "top");
-
-            for (int i = 0; i < operations.Length; i++)
-            {
-                string tabStops = i > 0 ? new string(' ', 14) : "   Type\t";
-                string centerThis = $"{tabStops}{i + 1}   - to {operations[i]}     \t{symbols[i]}    ";
-                Console.WriteLine(PrintCenteredTitle(centerThis, 29));
-            }
-            DisplayTitle("", "bottom");
-
-            Console.Write("Enter your choice \t: ");
-
-            return Console.ReadLine();
+          return result;
         }
-
-
-
-        public static bool ValidateInput(string input)
+        else
         {
-
-            Console.WriteLine(input);
-
-            try
-            {
-                int result = Convert.ToInt32(input);
-
-                if (result >= 1 && result <= 5)
-                {
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine($" Error: Enter choice between 1 and 5");
-                    return false;
-
-                }
-
-            }
-
-            catch (Exception err)
-            {
-                Console.WriteLine($" Error: {err.Message}");
-                Console.WriteLine(" Please enter a valid integer. Try again.");
-                return false;
-            }
-
-
-            //return true;
-        }
-
-        public static void DisplayTitle(string title, string cover)
-        {
-            string lineBoxTop = new string('─', 45);
-            string cornerLeftTop = " ┌";
-            string cornerRightTop = "┐ ";
-            string cornerLeftBottom = " └";
-            string cornerRightBottom = "┘ ";
-
-            string top = cornerLeftTop + lineBoxTop + cornerRightTop;
-            string bottom = cornerLeftBottom + lineBoxTop + cornerRightBottom;
-
-
-
-            if (cover == "all")
-            {
-                string middle = PrintCenteredTitle(title, 45);
-                ApplyHighlighter(top);
-                ApplyHighlighter(middle);
-                ApplyHighlighter(bottom);
-            }
-
-            if (cover == "top")
-            {
-                Console.WriteLine(top);
-            }
-
-            if (cover == "bottom")
-            {
-                Console.WriteLine(bottom);
-            }
+          Console.SetCursorPosition(position, Console.CursorTop - 1);
+          Console.WriteLine($"{ErrorColor} Error: Enter choice between 1 and 5  {ResetColor}");
+          return 0;
 
         }
 
+      }
 
-        static string PrintCenteredTitle(string title, int width)
-        {
-            int availableWidth = width;
-
-            string centeredTitle = string.Format(" │{0,-" + availableWidth + "}│ ",
-                title.PadLeft((availableWidth + title.Length) / 2).PadRight(availableWidth));
-            return centeredTitle;
-        }
+      catch
+      {
+        Console.SetCursorPosition(position, Console.CursorTop - 1);
+        Console.WriteLine($"{ErrorColor} Please enter a valid choice between 1 and 5. {ResetColor}");
+        return 0;
+      }
 
 
-        public static void ApplyHighlighter(string text)
-        {
-            string backgroundColor = "\u001B[48;2;26;132;184m";  // light blue
-            string foregroundColor = "\u001b[37m";  // white
-            string reset = "\u001b[39m\u001b[49m";
-            text = backgroundColor + foregroundColor + text + reset;
-            Console.WriteLine(text);
-        }
+      //return true;
+    }
+
+    public static void DisplayTitle(string title, string cover)
+    {
+      string lineBoxTop = new string('─', 45);
+      string cornerLeftTop = " ┌";
+      string cornerRightTop = "┐ ";
+      string cornerLeftBottom = " └";
+      string cornerRightBottom = "┘ ";
+
+      string top = cornerLeftTop + lineBoxTop + cornerRightTop;
+      string bottom = cornerLeftBottom + lineBoxTop + cornerRightBottom;
+
+
+
+      if (cover == "all")
+      {
+        string middle = PrintCenteredTitle(title, 45);
+        ApplyHighlighter(top);
+        ApplyHighlighter(middle);
+        ApplyHighlighter(bottom);
+      }
+
+      if (cover == "top")
+      {
+        Console.WriteLine(top);
+      }
+
+      if (cover == "bottom")
+      {
+        Console.WriteLine(bottom);
+      }
 
     }
+
+
+    static string PrintCenteredTitle(string title, int width)
+    {
+      int availableWidth = width;
+
+      string centeredTitle = string.Format(" │{0,-" + availableWidth + "}│ ",
+          title.PadLeft((availableWidth + title.Length) / 2).PadRight(availableWidth));
+      return centeredTitle;
+    }
+
+
+    public static void ApplyHighlighter(string text)
+    {
+      string backgroundColor = "\u001B[48;2;26;132;184m";  // light blue
+      string foregroundColor = "\u001b[37m";  // white
+      string reset = "\u001b[39m\u001b[49m";
+      text = backgroundColor + foregroundColor + text + reset;
+      Console.WriteLine(text);
+    }
+
+  }
 
 
 
